808a198d5335a0bd610833d027add7ca445955be
Deany256 <gdeandude@gmail.com>
2024-08-23 21:16:26 +0100

    Update routes.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,6 +2,10 @@ from flask import Blueprint
 
 auth_bp = Blueprint('auth', __name__)
 
+@auth_bp.route('/')
+def auth():
+    return "blank authentication page"
+
 @auth_bp.route('/login')
 def login():
     return "Login Page"
