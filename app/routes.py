2886325eb6df3b247bd8a87470e2a10d7eb982d9
Deany256 <gdeandude@gmail.com>
2024-08-25 01:21:57 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,8 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('main/index.html')
+
+@main_bp.route('/page')
+def page():
+    return "PAge"
