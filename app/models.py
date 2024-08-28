10fef3b91c394de4d2d16d429743dfa70b8f5f93
Deany256 <gdeandude@gmail.com>
2024-08-28 23:34:31 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,6 +6,20 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False, default=3)
+    
+    role = db.relationship('Role', backref='users')
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+    def is_admin(self):
+        return self.role_id == 1 
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    # users = db.relationship('User', backref='role', lazy=True)
+
+    def __repr__(self):
+        return f'<Role {self.name}>'
